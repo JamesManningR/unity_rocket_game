15b1f916a2107462df84e2146993fb182c4df970
jamesmanningr <jamesmanningr98@gmail.com>
2021-01-24 13:26:41 +0000

    Added rotation to rocket

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,29 +5,50 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    [SerializeField] float rcsThrust = 250f;
+    [SerializeField] float mainThrust = 40;
+
 
     // Start is called before the first frame update
-    void Start(){
+    void Start()
+    {
         rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update(){
-        ProcessInput();
+    void Update()
+    {
+        HandleThrust();
+        HandleRotation();
     }
 
-    private void ProcessInput() {
-        if (Input.GetKey(KeyCode.Space)) {
-            Thrust();
-        } else if (Input.GetKey(KeyCode.A)) {
-            print("Rorating left");
-        }
-        else if (Input.GetKey(KeyCode.D)) {
-            print("Rorating right");
+    private void HandleThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(0, mainThrust, 0);
         }
     }
 
-    private void Thrust() {
-        rigidBody.AddRelativeForce(0,20,0);
+    private void HandleRotation()
+    {
+        float rcsDirection = 0f;
+        float rotationInFrame = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigidBody.freezeRotation = true;
+            rcsDirection += 1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rigidBody.freezeRotation = true;
+            rcsDirection -= 1f;
+            print(rcsDirection);
+        }
+
+        // transform.Rotate(Vector3.forward * rcsDirection * rotationInFrame);
+
+        rigidBody.freezeRotation = false;
     }
 }
