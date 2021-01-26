5d18d97024231ca2c69518b763edb83214e2fbeb
jamesmanningr <jamesmanningr98@gmail.com>
2021-01-26 22:32:15 +0000

    Added scene navigation and reset

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -8,7 +8,8 @@ public class Rocket : MonoBehaviour
     [SerializeField] float mainThrust = 10000;
     [SerializeField] Vector3 centerOfMass = new Vector3(0, -.2f, 0);
 
-
+    enum State { Alive, Dying, Transcending };
+    State state = State.Alive;
 
     // Start is called before the first frame update
     void Start()
@@ -20,8 +21,20 @@ public class Rocket : MonoBehaviour
     void Update()
     {
         rigidBody.centerOfMass = centerOfMass;
-        HandleThrust();
-        HandleRotation();
+        switch (state)
+        {
+            case State.Alive:
+                HandleThrust();
+                HandleRotation();
+                HandleRestart();
+                break;
+
+            case State.Dying:
+                rigidBody.AddRelativeForce(0, mainThrust, 0);
+                float rotationInFrame = rcsThrust * Time.deltaTime;
+                rigidBody.angularVelocity += (Vector3.forward);
+                break;
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -32,12 +45,33 @@ public class Rocket : MonoBehaviour
                 break;
 
             case "Finish":
-                SceneManager.LoadScene(1);
+                state = State.Transcending;
+                LoadNextScene();
                 break;
 
             default:
-                SceneManager.LoadScene(0);
+                state = State.Dying;
+                ReloadScene();
                 break;
+        }
+    }
+
+    private void ReloadScene()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.name);
+    }
+
+    private void LoadNextScene()
+    {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        if (SceneManager.sceneCountInBuildSettings == nextSceneIndex)
+        {
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            SceneManager.LoadScene(nextSceneIndex);
         }
     }
 
@@ -72,5 +106,13 @@ public class Rocket : MonoBehaviour
         }
 
         rigidBody.angularVelocity += (Vector3.forward * rcsDirection * rotationInFrame);
+    }
+
+    private void HandleRestart()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            SceneManager.LoadScene(0);
+        }
     }
 }
