440ea1d63a4a31a0c4c1377f54ff68f6a4d466cd
jamesmanningr <jamesmanningr98@gmail.com>
2021-01-27 23:29:33 +0000

    Added sounds

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -3,10 +3,16 @@ using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
-    Rigidbody rigidBody;
     [SerializeField] float rcsThrust = 50f;
     [SerializeField] float mainThrust = 10000;
     [SerializeField] Vector3 centerOfMass = new Vector3(0, -.2f, 0);
+    [SerializeField] AudioClip mainEngineSound;
+    [SerializeField] AudioClip reverseEngineSound;
+    [SerializeField] AudioClip deathSound;
+
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
 
     enum State { Alive, Dying, Transcending };
     State state = State.Alive;
@@ -15,43 +21,51 @@ public class Rocket : MonoBehaviour
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
         rigidBody.centerOfMass = centerOfMass;
+
         switch (state)
         {
-            case State.Alive:
+            case State.Alive: // Check for inputs
                 HandleThrust();
                 HandleRotation();
                 HandleRestart();
                 break;
 
-            case State.Dying:
+            case State.Dying: // Spin out of control
                 rigidBody.AddRelativeForce(0, mainThrust, 0);
                 float rotationInFrame = rcsThrust * Time.deltaTime;
                 rigidBody.angularVelocity += (Vector3.forward);
+                ReloadScene();
+                break;
+
+            case State.Transcending:
+                LoadNextScene();
                 break;
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        // Ignore any collisions if player is dead or transcending
+        if (state != State.Alive) { return; }
+
         switch (collision.gameObject.tag)
-        {
+        { // Set state based oncollision type
             case "Friendly":
                 break;
 
             case "Finish":
                 state = State.Transcending;
-                LoadNextScene();
                 break;
 
             default:
                 state = State.Dying;
-                ReloadScene();
                 break;
         }
     }
@@ -77,24 +91,35 @@ public class Rocket : MonoBehaviour
 
     private void HandleThrust()
     {
-        float thrustDirection = 0f;
+        float thrustDirection = 0f; // Start thrust direction at 0
 
         if (Input.GetKey(KeyCode.Space))
         {
-            thrustDirection++;
-        }
-        if (Input.GetKey(KeyCode.LeftControl))
-        {
-            thrustDirection -= 0.5f;
+            if (!audioSource.isPlaying)
+            {
+                audioSource.PlayOneShot(mainEngineSound, 0.7f);
+            }
+            thrustDirection++; // Add to thrust in forward direction
         }
 
-        rigidBody.AddRelativeForce(0, thrustDirection * mainThrust, 0);
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            if (!audioSource.isPlaying)
+            {
+                audioSource.PlayOneShot(reverseEngineSound, 0.7f);
+            }
+            thrustDirection -= 0.5f; // Add to thrust in reverse direction
+        }
+
+        if (!Input.GetKey(KeyCode.LeftControl) && !Input.GetKey(KeyCode.Space)) { audioSource.Stop(); }
+
+        rigidBody.AddRelativeForce(0, thrustDirection * mainThrust, 0); // Apply final thrust direction
     }
 
     private void HandleRotation()
     {
         float rcsDirection = 0f;
-        float rotationInFrame = rcsThrust * Time.deltaTime;
+        float rotationInFrame = rcsThrust * Time.deltaTime; // Keep consistant RCS thrust regardless of frame rate
 
         if (Input.GetKey(KeyCode.A))
         {
